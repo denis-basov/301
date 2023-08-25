2db8b963377f0af8b03e1fbc97cfb41a789c16f4
Denis Basov <den4er@mail.ru>
2023-08-25 13:31:23 +0300

    асинхронный JS

--- 17 JS ASYNC/script.js
+++ 17 JS ASYNC/script.js
@@ -40,8 +40,81 @@
 // }
 
 // setTimeout
-console.log("Hello");
-setTimeout(function () {
-  console.log("winter");
-}, 2000);
-console.log("summer");
+// console.log("Hello");
+// setTimeout(function () {
+//   console.log("winter");
+// }, 2000);
+// console.log("summer");
+
+// fetch('https://jsonplaceholder.typicode.com/posts/1')
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+// // 1
+// fetch("https://randomfox.ca/floof/")
+//   .then((response) => response.json())
+//   .then((json) => document.write(`<img src="${json.image}">`));
+
+// // 2
+// console.log("hello");
+
+// fetch("https://restcountries.com/v3.1/all")
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+// получаем элемент для вставки данных
+let countryEl = document.querySelector("#country");
+
+// фукция для обработки данных данных и отображения в документе
+function showData(data) {
+  // console.log(data);
+
+  //1
+  // let borders = "";
+  // if (data.borders) {
+  //   borders = data.borders.join(", ");
+  // } else {
+  //   borders = "Соседних стран нет";
+  // }
+
+  //2
+  // if (data.borders) {
+  //   data.borders = data.borders.join(", ");
+  // } else {
+  //   data.borders = "Соседних стран нет";
+  // }
+
+  // формируем текст для вывода в документ
+  let output = `<div class="country">
+                  <div class="images">
+                    <img src="${data.flags.svg}" alt="${data.flags.alt}">
+                    <img src="${data.coatOfArms.png}" alt="Герб страны ${data.translations.rus.official}">
+                  </div>
+                  <h2>${data.translations.rus.official}</h2>
+                  <p>Столица: <span>${data.capital.join(", ")}</span></p>
+                  <p>Площадь: <span>${data.area.toLocaleString()} кв/км</span></p>
+                  <p>Население: <span>${data.population.toLocaleString()} чел.</span></p>
+                  <p>Регион: <span>${data.region ?? ""}</span></p>
+                  <p>Соседние страны: <span>${data.borders ? data.borders.join(", ") : "Соседних стран нет"}</span></p>
+                  <p>Временные зоны: <span>${data.timezones.join(", ")}</span></p>
+                </div>`;
+
+  countryEl.insertAdjacentHTML("beforeend", output);
+}
+
+// функция для получения данных с сервера
+async function whereIAm(country) {
+  let response = await fetch(`https://restcountries.com/v3.1/name/${country}`); // отправляем запрос на внешний сервер
+  let data = await response.json(); // получаем данные
+
+  // показываем данные в документе
+  showData(data[0]);
+}
+whereIAm("russia");
+whereIAm("peru");
+whereIAm("australia");
+whereIAm("Papua");
+
+/*
+characters = requests.get("https://rickandmortyapi.com/api/character?page=1").json()["results"]
+*/
---
+++ 18 PHP основы/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- контейнер для пользователей -->
+    <div class="users">
+        <!-- пользователь 1 -->
+        <div class="user user-1">
+            <h2>Иван Иванов</h2>
+            <p>Возраст: 22</p>
+            <p>Страна проживания: Россия</p>
+            <p>Хобби: Гулять</p>
+        </div>
+        <!-- пользователь 2 -->
+        <div class="user user-2">
+            <h2>Ирина Иванова</h2>
+            <p>Возраст: 21</p>
+            <p>Страна проживания: Россия</p>
+            <p>Хобби: Вязать</p>
+        </div>
+        <!-- пользователь 3 -->
+        <div class="user user-3">
+            <h2>Анна Петрова</h2>
+            <p>Возраст: 20</p>
+            <p>Страна проживания: Россия</p>
+            <p>Хобби: Варить борщ</p>
+        </div>
+    </div>
+</body>
+
+</html>
