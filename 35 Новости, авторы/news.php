26a916c1064e03599a650169873173ba2c73d12f
Denis Basov <den4er@mail.ru>
2023-09-08 13:33:06 +0300

    новости, авторы

--- 35 Новости, авторы/news.php
+++ 35 Новости, авторы/news.php
@@ -13,7 +13,6 @@ $query = "SELECT news.id AS newsId, news.title, add_date, image,
             ORDER BY add_date DESC;";
 $statement = $pdo->query($query);
 $newsList = $statement->fetchAll();
-print_r($newsList);
 ?>
 <!doctype html>
 <html lang="en">
@@ -23,23 +22,32 @@ print_r($newsList);
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Список новостей</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container">
         <h1>Список новостей</h1>
+        <a href="/">Главная</a>
+        <a href="authors.php">Авторы</a>
         <div class="news-list">
             <?php foreach ($newsList as $newsItem):?>
                 <div class="news-item">
                     <div class="news-img">
-                        <img src="image" alt="title">
+                        <a href="news_detail.php?newsId=<?=$newsItem['newsId']?>">
+                            <img src="<?=trim($newsItem['image'], '/')?>" alt="<?=$newsItem['title']?>">
+                        </a>
                     </div>
                     <div class="news-info">
-                        <h2>title</h2>
+                        <h2><?=$newsItem['title']?></h2>
                         <div>
-                            <span>add_date</span>
-                            <span>first_name</span>
-                            <span>last_name</span>
-                            <span>category</span>
+                            <p>Дата публикации: <?=$newsItem['add_date']?></p>
+                            <p>Автор:
+                                <a href="author_detail.php?authorId=<?=$newsItem['authorId']?>">
+                                    <?=$newsItem['first_name']?> <?=$newsItem['last_name']?>
+                                </a>
+                            </p>
+                            <p class="<?=$newsItem['category_class_name']?>">Категория: <?=$newsItem['category']?></p>
+                            <a href="news_detail.php?newsId=<?=$newsItem['newsId']?>">Подробнее...</a>
                         </div>
                     </div>
                 </div>
--- 35 Новости, авторы/news_detail.php
+++ 35 Новости, авторы/news_detail.php
@@ -1,2 +1,51 @@
 <?php
 // отображение одной новости детально
+
+require 'DBConnect.php';
+$pdo = DBConnect::getConnection();
+
+// забираем ID новости из массива $_GET
+$newsId = (int)$_GET['newsId'];
+
+$query = "SELECT news.title, text, add_date, image,
+            authors.id AS authorId, first_name, last_name, short_info, avatar,
+            translation AS category, class_name AS category_class_name
+            FROM news, authors, category
+            WHERE author_id = authors.id AND
+            category_id = category.id AND
+            news.id = $newsId;";
+$statement = $pdo->query($query);
+$newsItem = $statement->fetch();
+
+//print_r($newsItem);
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?=$newsItem['title']?></title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="news-item">
+            <div class="news-info">
+                <h1><?=$newsItem['title']?></h1>
+                <img src="<?=$newsItem['image']?>" alt="<?=$newsItem['title']?>">
+                <p><?=$newsItem['text']?></p>
+                <p>Дата публикации: <?=$newsItem['add_date']?></p>
+                <p class="<?=$newsItem['category_class_name']?>">Категория: <?=$newsItem['category']?></p>
+            </div>
+            <div class="author-info">
+                <img src="<?=$newsItem['avatar']?>" alt="<?=$newsItem['first_name']?> <?=$newsItem['last_name']?>">
+                <p><a href="author_detail.php?authorId=<?=$newsItem['authorId']?>"><?=$newsItem['first_name']?> <?=$newsItem['last_name']?></a></p>
+                <p><?=$newsItem['short_info']?></p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
