d981ea2072314b3dba0009a1628dc3f48876efd4
Denis Basov <den4er@mail.ru>
2023-09-08 10:23:13 +0300

    новости, авторы

--- 34 Вывод данны в документ через JS/script.js
+++ 34 Вывод данны в документ через JS/script.js
@@ -1,12 +1,40 @@
 let booksContainer = document.querySelector('#books');// получаем элемент для вставки в него новостей
 
 /**
+ * функция для формирования и вывода книг в документ
+ */
+function showBooksList(books){
+
+    // формируем из массива объектов строку с разметкой
+    let output = books.map(function(book){
+        return `
+            <div class="book">
+                <div class="book-info">
+                    <h3>${book.title}</h3>
+                    <p>${book.description}</p>
+                    <a href="book_detail.php?bookId=${book.bookId}">Подробнее...</a>
+                </div>
+                <div class="author-info">
+                    <a href="author_detail.php?authorId=${book.authorId}">
+                        <p>Автор: ${book.firstName} ${book.lastName}</p>
+                    </a>
+                </div>
+            </div>
+        `;
+    }).join('');
+
+    // отправляем сформированную разметку в документ последним потомком
+    booksContainer.insertAdjacentHTML('beforeend', output);
+}
+
+
+/**
  * функция для получения списка книг
  */
 async function getBooksList(){
     let response = await fetch(`server/getBooksList.php`);
-    console.log(response);
     let books = await response.json();
-    console.log(books);
+
+    showBooksList(books);
 }
 getBooksList();
--- 34 Вывод данны в документ через JS/server/getBooksList.php
+++ 34 Вывод данны в документ через JS/server/getBooksList.php
@@ -1,3 +1,21 @@
 <?php
 
-echo json_encode(["message" => 'Hello from server', "login" => 'admin']);
+require 'DBConnect.php';
+$pdo = DBConnect::getConnection();
+
+$query = "SELECT books.id AS bookId, title, description, authors.id AS authorId, firstName, lastName
+            FROM books, authors
+            WHERE authorId = authors.id;";
+
+$statement = $pdo->query($query);
+$books = $statement->fetchAll();
+
+/*
+$book = ['bookId' => 11, 'title' => 'Кавказский пленник'];
+array_push($data, $book);
+print_r($data);
+*/
+
+// кодируем в JSON и отдаем в ответ
+echo json_encode($books);
+
